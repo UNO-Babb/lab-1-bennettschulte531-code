1799f9fb194f2c2f57e5c45dea4c2561adf194c4
bennettschulte531-code <bennettschulte531@gmail.com>
2025-08-29 20:15:03 +0000

    done with Lab1

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,22 +1,23 @@
 #FirstProgram.py
-#Name:
-#Date:
-#Assignment:
+#Name:Bennett Schulte
+#Date:8/26/25
+#Assignment:Lab 1
 
 def main():
   print("First Program")
   #Say hello
-  
+  print("hello")
   #Ask for the user's name
-
+username = input("What is your name?")
   #Use the user's name in the program.
-
+print("Nice to meet you",(username)+"!")
   #Ask the user for their age.
-
+user_age = int(input("How old are you?"))
   #Tell the user what year they were born in.
   #Assume that they have not had their birthday yet this year.
-
-
+current_year = 2025
+birth_year = current_year - user_age
+print("You were born in" , (birth_year))
 #Call the main function if this is the file being run.
 if __name__ == '__main__':
     main()
--- MadLib.py
+++ MadLib.py
@@ -1,15 +1,22 @@
 #MadLib.py
-#Name:
+#Name:Bennett Schulte
 #Date:
-#Assignment:
+#Assignment:MadLib
 
 def main():
   print("Madlib")
   #Ask user for words
+verb1 = input("Enter a verb: ")
+verb2 = input("Enter a verb: ")
+noun1 = input("Enter a noun: ")
+noun2 = input("Enter a noun: ")
+adjective1 = input("Enter an adjective: ")
+adjective2 = input("Enter an adjective: ")
+
 
   #Print the story with the user supplied words.
 
-
+print(f"The {adjective1} {noun1} wanted to {verb1} quickly. Then the {adjective2} {noun2} began to {verb2} suddenly.")
 
 #Call the main function if this is the file being run.
 if __name__ == '__main__':
